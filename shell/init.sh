e39ca8374dbe4be9a3fd05d0b72dfd527bd6a5b1
Daniel Enriquez <daniel.enm17@gmail.com>
2022-09-23 21:34:37 -0500

    Folders

--- shell/init.sh
+++ shell/init.sh
@@ -1,10 +1,10 @@
 alias sudo='sudo '
 
 # register all aliases and functions
-for aliasToSource in "$HOME/.dotfiles/shell/_aliases/"*; do
+for aliasToSource in "$HOME/.dotfiles/shell/lib/_aliases/"*; do
   source "$aliasToSource"
 done
 
-for functionToSource in "$HOME/.dotfiles/shell/_functions/"*; do
+for functionToSource in "$HOME/.dotfiles/shell/lib/_functions/"*; do
   source "$functionToSource"
 done
