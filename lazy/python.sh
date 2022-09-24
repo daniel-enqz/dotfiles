5bd68b26dce2e74316b6a2f96b50aaa552bcbcc9
Daniel Enriquez <daniel.enm17@gmail.com>
2022-09-24 13:44:55 -0500

    Python moved

--- lazy/python.sh
+++ lazy/python.sh
@@ -1,4 +1,6 @@
 function load_python() {
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
   # Load pyenv (to manage your Python versions)
   export PYENV_VIRTUALENV_DISABLE_PROMPT=1
   type -a pyenv > /dev/null && eval "$(pyenv init -)" && eval "$(pyenv virtualenv-init -)" && RPROMPT+='[🐍 $(pyenv version-name)]'
