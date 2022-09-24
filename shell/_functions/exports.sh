18675526f12ec83fd3dc5b021b0182e24eb71ffc
Daniel Enriquez <daniel.enm17@gmail.com>
2022-09-23 21:30:12 -0500

    Folders

--- shell/_functions/exports.sh
+++ shell/_functions/exports.sh
@@ -1,4 +1,4 @@
-function export_apps() {
+function export_packages() {
   brew services stop --all
   brew bundle dump --file="$DOTFILES_DIR"/exports/brew/Brewfile --force
   echo "Brew apps exported"
@@ -8,6 +8,6 @@ function export_apps() {
   echo "Pip apps exported"
 
   rm -rf "$DOTFILES_DIR"/exports/npm/npm.txt
-  npm list --location=global --depth=0 | grep -v npm > "$DOTFILES_DIR"/exports/npm/npm.txt
+  ls -1 /Users/daniel-enqz/.nvm/versions/node/v16.15.1/lib/node_modules | grep -v npm > "$DOTFILES_DIR"/exports/npm/npm.txt
   echo "Npm apps exported"
 }
