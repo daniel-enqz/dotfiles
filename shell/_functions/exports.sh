ed01bb2cb725a12727481e79de400af7f9994ff4
Daniel Enriquez <daniel.enm17@gmail.com>
2022-09-23 20:50:20 -0500

    Exports

--- shell/_functions/exports.sh
+++ shell/_functions/exports.sh
@@ -3,9 +3,11 @@ function export_apps() {
   brew bundle dump --file="$DOTFILES_DIR"/exports/brew/Brewfile --force
   echo "Brew apps exported"
 
+  rm -rf "$DOTFILES_DIR"/exports/pip/requirements.txt
   pip freeze > "$DOTFILES_DIR"/exports/pip/requirements.txt
   echo "Pip apps exported"
 
+  rm -rf "$DOTFILES_DIR"/exports/npm/npm.txt
   npm list --location=global --depth=0 | grep -v npm > "$DOTFILES_DIR"/exports/npm/npm.txt
   echo "Npm apps exported"
 }
