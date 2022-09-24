96ffbed0e40fb7de9e8415a8458045caca212858
Daniel Enriquez <daniel.enm17@gmail.com>
2022-09-23 21:44:20 -0500

    Comment added

--- shell/lib/_functions/exports.sh
+++ shell/lib/_functions/exports.sh
@@ -9,5 +9,6 @@ function export_packages() {
 
   rm -rf "$DOTFILES_DIR"/exports/npm/npm.txt
   ls -1 /Users/daniel-enqz/.nvm/versions/node/v16.15.1/lib/node_modules | grep -v npm > "$DOTFILES_DIR"/exports/npm/npm.txt
+  # npm list -location=global --depth 0
   echo "Npm apps exported"
 }
