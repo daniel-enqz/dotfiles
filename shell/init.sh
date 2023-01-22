aeb549f847f9ddc0d3bba8979115761a554447de
Daniel Enriquez <daniel.enm17@gmail.com>
2023-01-22 00:27:36 -0600

    fix for shell check

--- shell/init.sh
+++ shell/init.sh
@@ -4,11 +4,9 @@ alias sudo='sudo '
 
 # register all aliases and functions
 for aliasToSource in "$DOTFILES_DIR/shell/lib/_aliases/"*; do
-  # shellcheck source=./shell/lib/_aliases/alias.sh
   source "$aliasToSource"
 done
 
 for functionToSource in "$DOTFILES_DIR/shell/lib/_functions/"*; do
-  # shellcheck source=./shell/lib/_functions/function.sh
   source "$functionToSource"
 done
