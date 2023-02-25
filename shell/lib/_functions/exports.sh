034f734ace5783b20cbb667912f2b27b7bfb0eea
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-24 23:18:29 -0600

    exports working well

--- shell/lib/_functions/exports.sh
+++ shell/lib/_functions/exports.sh
@@ -5,19 +5,24 @@ function export_packages() {
   brew bundle dump --file="$DOTFILES_DIR"/exports/brew/Brewfile --force
   echo "Brew apps exported"
 
+  ipython
   rm -rf "$DOTFILES_DIR"/exports/pip/requirements.txt
   pip freeze > "$DOTFILES_DIR"/exports/pip/requirements.txt
   echo "Pip apps exported"
 
-  rm -rf "$DOTFILES_DIR"/exports/npm/npm.txt
-  # ls -1 /Users/daniel-enqz/.nvm/versions/node/v16.15.1/lib/node_modules | grep -v npm > "$DOTFILES_DIR"/exports/npm/npm.txt
-  # npm list -location=global --depth 0
-  for file in /Users/daniel-enqz/.nvm/versions/node/v16.15.1/lib/node_modules/*
-  do
-    if [[ "$file" != *"npm"* ]]; then
-      echo "$file" >> "$DOTFILES_DIR"/exports/npm/npm.txt
-    fi
-  done
 
+  rm -rf "$DOTFILES_DIR"/exports/npm/npm.txt
+  ls -1 /Users/daniel-enqz/.nvm/versions/node/v18.13.0/lib/node_modules | grep -v npm > "$DOTFILES_DIR"/exports/npm/npm.txt
   echo "Npm apps exported"
 }
+
+# NPM Alternatives:
+
+# for file in /Users/daniel-enqz/.nvm/versions/node/v18.13.0/lib/node_modules/*
+# do
+#   if [[ "$file" != *"npm"* ]]; then
+#     echo "$file" >> "$DOTFILES_DIR"/exports/npm/npm.txt
+#   fi
+# done
+
+# npm list -location=global --depth 0
