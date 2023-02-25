6a6802f2a70fef30765db79f889b322c209e1747
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-25 16:41:58 -0600

    add new tmux aliases

--- shell/lib/_functions/exports.sh
+++ shell/lib/_functions/exports.sh
@@ -12,17 +12,7 @@ function export_packages() {
 
 
   rm -rf "$DOTFILES_DIR"/exports/npm/npm.txt
+  # npm list -location=global --depth 0
   ls -1 /Users/daniel-enqz/.nvm/versions/node/v18.13.0/lib/node_modules | grep -v npm > "$DOTFILES_DIR"/exports/npm/npm.txt
   echo "Npm apps exported"
 }
-
-# NPM Alternatives:
-
-# for file in /Users/daniel-enqz/.nvm/versions/node/v18.13.0/lib/node_modules/*
-# do
-#   if [[ "$file" != *"npm"* ]]; then
-#     echo "$file" >> "$DOTFILES_DIR"/exports/npm/npm.txt
-#   fi
-# done
-
-# npm list -location=global --depth 0
