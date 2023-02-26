b059897d4643fab2ac5c2f731684393e4712083a
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-26 09:39:42 -0600

    feat<alias>add tmux confing at dotfiles and symbolic link

--- startup/lib/link_files.sh
+++ startup/lib/link_files.sh
@@ -3,10 +3,11 @@
 TEMP_DOTFILES_DIR=/Users/daniel-enqz/.dotfiles
 
 ln -s "$TEMP_DOTFILES_DIR/shell/zsh/.zshenv" "$HOME/.zshenv"
-ln -s "$DOTFILES_DIR/git/.gitconfig" "$HOME/.gitconfig"
 ln -s "$DOTFILES_DIR/lazy/" "$HOME/.gitignore_global"
 ln -s "$DOTFILES_DIR/shell/zsh/.hushlogin" "$HOME/.hushlogin"
 ln -s "$DOTFILES_DIR/shell/zsh/.zshrc" "$HOME/.zshrc"
 ln -s "$DOTFILES_DIR/shell/zsh/.zprofile" "$HOME/.zprofile"
 ln -s "$DOTFILES_DIR/shell/zsh/.zimrc" "$HOME/.zimrc"
 ln -s "$DOTFILES_DIR/shell/zsh/.zim" "$HOME/.zim"
+ln -s "$DOTFILES_DIR/config/git/.gitconfig" "$HOME/.gitconfig"
+ln -s "$DOTFILES_DIR/config/tmux/.tmux.conf" "$HOME/.tmux.conf"
