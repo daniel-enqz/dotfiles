0331766423c448bdc18b63732c77c706c2e69cd8
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-27 11:28:24 -0600

    new plugin added (telescope)

--- config/nvim/lua/daniel-enqz/plugins-setup.lua
+++ config/nvim/lua/daniel-enqz/plugins-setup.lua
@@ -54,6 +54,10 @@ return packer.startup(function(use)
   -- statusline
   use("nvim-lualine/lualine.nvim")
 
+  -- fuzzy finding w/ telescope
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+
   if packer_bootstrap then
     require("packer").sync()
   end
--- config/nvim/plugin/packer_compiled.lua
+++ config/nvim/plugin/packer_compiled.lua
@@ -104,6 +104,16 @@ _G.packer_plugins = {
     path = "/Users/daniel-enqz/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["telescope-fzf-native.nvim"] = {
+    loaded = true,
+    path = "/Users/daniel-enqz/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
+    url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/Users/daniel-enqz/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
   ["vim-ReplaceWithRegister"] = {
     loaded = true,
     path = "/Users/daniel-enqz/.local/share/nvim/site/pack/packer/start/vim-ReplaceWithRegister",
