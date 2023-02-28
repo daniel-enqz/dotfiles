de8b88d6e61d3359547dfe29f72bdacb87954415
Daniel Enriquez <daniel.enm17@gmail.com>
2023-02-27 20:45:27 -0600

    add github copilot

--- config/nvim/lua/daniel-enqz/plugins-setup.lua
+++ config/nvim/lua/daniel-enqz/plugins-setup.lua
@@ -103,6 +103,9 @@ return packer.startup(function(use)
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+	-- copilot
+	use("github/copilot.vim")
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
