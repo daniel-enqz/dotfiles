839ba08f941ab53a57d533eb72b11bf598578a75
Daniel Enriquez <daniel.enm17@gmail.com>
2023-05-01 13:14:06 -0600

    add vtr config and instructions to lua

--- config/nvim/lua/daniel-enqz/plugins/rspec.lua
+++ config/nvim/lua/daniel-enqz/plugins/rspec.lua
@@ -3,13 +3,20 @@ if not setup then
 	return
 end
 
+-- If you have a tmux runner pane attached to your vim session, you can send the current line to it with <leader>trl.
+-- For sending rspec tests you need to enable the next in setup
 rspec.setup({
-	command = {
-		vim = {
-			"call VtrSendCommand('rspec {spec}')",
-		},
-		nvim = {
-			"lua require('rspec').run()",
-		},
-	},
+	on_start = function()
+		vim.cmd("VtrAttachToPane")
+	end,
+	on_job_complete = function()
+		vim.cmd("VtrDetach")
+	end,
+	file_pattern = "%w+_%d+_spec.rb",
+	command = "bundle exec rspec",
+	closest_file = function()
+		return vim.fn.expand("%:p"):match("spec/.+_spec")
+	end,
 })
+
+-- Now if you want to send the current spec to the runner pane, you can use <leader>rsf.
