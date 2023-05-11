7c164f6ba88498e3888212b15d12fe10b95a1e79
Daniel Enriquez <daniel.enm17@gmail.com>
2023-05-11 12:59:12 -0600

    add new config for tab toggle

--- config/nvim/lua/daniel-enqz/plugins/barbar.lua
+++ config/nvim/lua/daniel-enqz/plugins/barbar.lua
@@ -4,3 +4,7 @@ local opts = { noremap = true, silent = true }
 -- Move to previous/next
 map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
 map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
+
+-- keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+-- keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+-- keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
