6ab0aba8167c59b0c9574d3578a5864039b789c0
Daniel Enriquez <daniel.enm17@gmail.com>
2023-05-12 09:26:41 -0600

    add config for bar bar

--- config/nvim/lua/daniel-enqz/plugins/barbar.lua
+++ config/nvim/lua/daniel-enqz/plugins/barbar.lua
@@ -2,9 +2,6 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
-map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
-
--- keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
--- keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
--- keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+map("n", "<leader>,", "<Cmd>BufferPrevious<CR>", opts)
+map("n", "<leader>.", "<Cmd>BufferNext<CR>", opts)
+map("n", "<leader>..", "<Cmd>BufferClose<CR>", opts)
