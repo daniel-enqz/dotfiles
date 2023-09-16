006049dc2d38c1f6cbcc7685e0c4920a35415f1d
Daniel Enriquez <daniel.enm17@gmail.com>
2023-09-16 16:30:08 -0600

    Add new catpuccin config

--- config/nvim/lua/daniel-enqz/core/colorscheme.lua
+++ config/nvim/lua/daniel-enqz/core/colorscheme.lua
@@ -1,5 +1,9 @@
 local status, _ = pcall(vim.cmd.colorscheme, "catppuccin")
 if not status then
-  print("Colorscheme not found!")
-  return
+	print("Colorscheme not found!")
+	return
 end
+
+require("catppuccin").setup({
+	flavour = "mocha", -- latte, frappe, macchiato, mocha
+})
