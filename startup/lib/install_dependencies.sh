b4c19a6cc76779e9cb9c93018a667607adc34510
Daniel Enríquez <daniel-enqz@Daniels-MacBook-Pro.local>
2024-03-17 03:12:06 -0600

    fix config

--- startup/lib/install_dependencies.sh
+++ startup/lib/install_dependencies.sh
@@ -9,4 +9,6 @@ brew install neovim
 brew install tmux
 brew install make
 
+# LunarVim
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+cp ~/.local/share/lunarvim/lvim/utils/installer/config.example.lua ~/.config/lvim/config.lua
