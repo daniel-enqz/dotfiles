214c1f1ad11c781e34996903c456a4ae6dde3f75
Daniel Enriquez <daniel.enm17@gmail.com>
2024-03-17 02:29:23 -0600

    add install dependencies

--- startup/lib/install_dependencies.sh
+++ startup/lib/install_dependencies.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
+getnf
+
+brew install navi
+brew install neovim
+brew install tmux
+brew install make
