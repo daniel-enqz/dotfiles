39aa29984103247d8f97a8ddcc6e5370712b3d20
Daniel Enriquez <daniel.enm17@gmail.com>
2024-03-17 16:17:53 -0600

    Set default Python breakpoint to ipdb

--- lazy/python.sh
+++ lazy/python.sh
@@ -6,4 +6,6 @@ function load_python() {
   # Load pyenv (to manage your Python versions)
   export PYENV_VIRTUALENV_DISABLE_PROMPT=1
   type -a pyenv > /dev/null && eval "$(pyenv init -)" && eval "$(pyenv virtualenv-init -)" && RPROMPT+='[🐍 $(pyenv version-name)]'
+  
+  export PYTHONBREAKPOINT=ipdb.set_trace
 }
