aeeac49c75c1ca03fbac80f3b9156e2cc700d618
Daniel Enriquez <daniel.enm17@gmail.com>
2024-03-17 12:04:43 -0600

    better config

--- shell/lib/_functions/git.sh
+++ shell/lib/_functions/git.sh
@@ -79,6 +79,15 @@ function ga {
       commit_message=$(generate_commit_message "$diff_data")
       commit_changes "$commit_message"
   elif [[ "$1" == "--squash" ]]; then
+      local base_branch="main"
+      local current_branch=$(git rev-parse --abbrev-ref HEAD)
+      printf "Current branch: $current_branch\n"
+      printf "Base branch: $base_branch\n"
+      if [ "$current_branch" == "$base_branch" ]; then
+          echo "You're on the base branch ($base_branch). Switch to a feature branch to use this command."
+          return
+      fi
+
       commit_messages=$(get_commit_messages_since_divergence)
       if [ -z "$commit_messages" ]; then
           echo "No new commits to summarize."
