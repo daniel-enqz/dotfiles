dcbae492bd4a4c902a2fd760e7dac74f922d405c
Daniel Enriquez <daniel.enm17@gmail.com>
2024-03-17 11:39:50 -0600

    Refactor git shell functions to use OpenAI for generating commit messages and adjust OPENAI_API_KEY in .zshenv

--- shell/lib/_functions/git.sh
+++ shell/lib/_functions/git.sh
@@ -1,26 +1,54 @@
-#!/bin/bash
-
-# ----------------- Git -----------------
 function ga {
-  git add .
+  get_diff_data() {
+      git diff
+  }
 
-  # Ask the user to select a type:
-  PS3="🍀 Select type: "
-  select choice in "feat" "fix" "chore" "docs" "ref" "style" "test"
-  do
-      scope="$choice"
-      break;
-  done
+  generate_commit_message() {
+      local diff_data="$1"
+      # Use jq to properly escape and encode the diff data as a JSON string
+      local json_diff_data=$(jq -aRs . <<< "$diff_data")
 
-  echo "🧼 Description:"
-  read -r description
+      # Prepare the data for sending as JSON, correctly incorporating json_diff_data
+      local data_json=$(jq -cn --arg jsonDiffData "$json_diff_data" '{
+        "model": "gpt-4-turbo-preview",
+        "messages": [
+          {
+            "role": "system",
+            "content": "You are a commit message assistant. Your task is to generate a concise and informative commit message based on the provided git diff data."
+          },
+          {
+            "role": "user",
+            "content": $jsonDiffData
+          }
+        ]
+      }')
 
-  # Construct the commit message and commit the changes
-  git commit -m "<${scope}>${description}"
+      # Make the call to OpenAI's Chat API
+      local response=$(curl -s \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $OPENAI_API_KEY" \
+        -d "$data_json" \
+        "https://api.openai.com/v1/chat/completions")
 
-}
+      # Extract the text from the response, ensuring to parse the correct field
+      echo $response | jq -r '.choices[0].message.content'
+  }
 
-function gfcm {
-  # find a commit by its message
-  git log --all --grep="$1"
+  commit_changes() {
+      echo "Ready to commit with the following message:"
+      echo "git commit -m \"$1\""
+      echo "Press Enter to proceed or Ctrl+C to abort."
+      read
+      git add .
+      git commit -m "$1"
+  }
+
+  diff_data=$(get_diff_data)
+  commit_message=$(generate_commit_message "$diff_data")
+
+  if [[ "$1" == "--commit" ]]; then
+      commit_changes "$commit_message"
+  else
+      echo "To commit these changes, run the script with the --commit flag."
+  fi
 }
--- shell/zsh/.zshenv
+++ shell/zsh/.zshenv
@@ -3,4 +3,4 @@ export PATH="$HOME/.local/bin:$PATH"
 export NAVI_PATH=/Users/daniel-enqz/.dotfiles/config/navi
 export DOTFILES_DIR=/Users/daniel-enqz/.dotfiles
 export EDITOR='lvim'
-export OPENAI_API_KEY="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"
+export OPENAI_API_KEY="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"
