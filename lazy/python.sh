56c24d31e473b2d7dae43f9286e27d44ff07afac
Daniel Enriquez <daniel.enm17@gmail.com>
2024-08-04 10:01:23 -0600

    remove snake from python env

--- lazy/python.sh
+++ lazy/python.sh
@@ -5,7 +5,7 @@ function load_python() {
   export PATH="$PYENV_ROOT/bin:$PATH"
   # Load pyenv (to manage your Python versions)
   export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-  type -a pyenv > /dev/null && eval "$(pyenv init -)" && eval "$(pyenv virtualenv-init -)" && RPROMPT+='[🐍 $(pyenv version-name)]'
+  type -a pyenv > /dev/null && eval "$(pyenv init -)" && eval "$(pyenv virtualenv-init -)"
   # Set ipdb as the default Python debugger
   export PYTHONBREAKPOINT=ipdb.set_trace
 }
