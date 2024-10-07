3157215fc8a49d59ddc08ea00fc00f4a8b4b42a9
Daniel Enriquez <daniel.enm17@gmail.com>
2024-10-07 14:39:01 -0600

    add sample test flutter

--- shell/lib/_functions/efficiency.sh
+++ shell/lib/_functions/efficiency.sh
@@ -3,3 +3,22 @@
 function ltt {
   for _ in $(seq 1 10); do time zsh -i -c exit; done
 }
+
+function run_dart_test {
+  if [ -z "$1" ]; then
+    echo "Usage: run_dart_test <test_file_path>"
+    return 1
+  fi
+
+  # Store the current directory
+  current_dir=$(pwd)
+
+  # Change to the test/bdc directory
+  cd test/bdc || { echo "Failed to change to test/bdc directory"; return 1; }
+
+  # Run the Flutter test
+  flutter test --no-sound-null-safety "$1"
+
+  # Return to the original directory
+  cd "$current_dir" || { echo "Failed to return to original directory"; return 1; }
+}
